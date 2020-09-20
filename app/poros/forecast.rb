fe86423fe799c84f6cef742f0e89cb5944df9dd8
Josh Tukman <josh.tukman@gmail.com>
2020-09-19 19:41:16 -0600

    Create main forecast poro with all required attributes based on wireframes for project

---
+++ app/poros/forecast.rb
@@ -0,0 +1,30 @@
+require 'date'
+class Forecast
+  def initialize(weather_info)
+    @time = day_time(weather_info[:current][:dt])
+    @current_temp = weather_info[:current][:temp]
+    @feels_like = weather_info[:current][:feels_like]
+    @humidity = weather_info[:current][:humidity]
+    @visibility = weather_info[:current][:visibility]
+    @uv_index = weather_info[:current][:uvi]
+    @sunrise = Time.at(weather_info[:current][:sunrise]).strftime('%-I:%M %p')
+    @sunset = Time.at(weather_info[:current][:sunset]).strftime('%-I:%M %p')
+    @high_temp = weather_info[:daily][0][:temp][:max]
+    @low_temp = weather_info[:daily][0][:temp][:min]
+    @hourly = hourly_forcast(weather_info[:hourly][0..23])
+    @five_day_forecast = five_day_forecast(weather_info[:daily][1..5])
+  end
+
+  def hourly_forcast(report)
+    report.map{|day_info| HourlyForecast.new(day_info)}
+  end
+
+  def day_time(utc)
+    Time.at(utc).strftime('%-I:%M %p, %B %-d')
+  end
+
+  def five_day_forecast(info)
+   info.map{|day_info| FiveDayForecast.new(day_info)}
+  end
+
+end
