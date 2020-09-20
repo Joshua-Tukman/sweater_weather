2427511a05928bf0a3d81aa1485f4917391e4c37
Josh Tukman <josh.tukman@gmail.com>
2020-09-20 14:55:57 -0600

    Remove unneccessary WeatherService.new

--- app/poros/hourly_forecast.rb
+++ app/poros/hourly_forecast.rb
@@ -3,7 +3,6 @@ class HourlyForecast
   attr_reader :temp,
               :time
   def initialize(info)
-    @weather_service = WeatherService.new
     @temp = info[:temp].to_i
     @time = Time.at(info[:dt]).strftime('%-I %p')
   end
