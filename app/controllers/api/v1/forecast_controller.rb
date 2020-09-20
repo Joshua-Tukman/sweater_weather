880854bb2c6047735b6dc533e5f4589bc06f701f
Josh Tukman <josh.tukman@gmail.com>
2020-09-20 14:49:06 -0600

    End index action by sending forecast object through serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = params[:location]
-    @forecast = ForecastFacade.new(location) 
+    forecast = ForecastFacade.new(location)
+    render json: ForecastSerializer.new(forecast)
   end
 end 
