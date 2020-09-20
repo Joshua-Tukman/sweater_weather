a46bd88caf4e23b0e13205313baacd72bc75c46d
Josh Tukman <josh.tukman@gmail.com>
2020-09-20 14:51:22 -0600

    Add validation tests for location both city and state params

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,10 +1,11 @@
 class ForecastFacade
+  attr_reader :forecast, :id
   def initialize(location)
-    @location = location
+    @location = location_format(location)
     @map_quest_service = MapQuestService.new
     @weather_service = WeatherService.new
-    @lat_long = lat_long_formatted
-    @weather = current_weather(lat_long_formatted)
+    @forecast = current_weather(lat_long_formatted)
+    @id = nil
   end
 
   def lat_long_formatted
@@ -12,7 +13,44 @@ class ForecastFacade
   end
   
   def current_weather(lat_long)
-    Forecast.new(@weather_service.forecast(lat_long))
+    if @location == {:error=>"Invalid City"}
+      {:error=> "Invalid City"}
+    elsif @location == {:error=>"Invalid State"}
+      {:error=> "Invalid State"}
+    else
+      Forecast.new(@weather_service.forecast(lat_long))
+    end 
   end
-   
+
+  def location_format(location)
+    city = location.split(',')[0].capitalize
+    state = location.split(',')[1].upcase
+    validate_state(state)
+    if validate_state(state) == {:error=>"Invalid State"}      
+      return {:error=>"Invalid State"}
+    else 
+      validate_city(city, state)
+        if validate_city(city, state) == {:error=>"Invalid City"}
+          {:error=>"Invalid City"}
+        else   
+          "#{city}, #{state}"
+        end 
+    end 
+  end
+
+  def validate_state(state)
+    if CS.states(:us).include?(state.to_sym)
+      state.to_s
+    else
+      Errors.new.invalid_state
+    end
+  end
+
+  def validate_city(city, state)
+    if CS.cities(state.to_sym, :us).include?(city)
+      city
+    else
+      Errors.new.invalid_city
+    end
+  end    
 end 
