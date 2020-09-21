273d77a68140ca8500fbcd0b17881e094e262603
Josh Tukman <josh.tukman@gmail.com>
2020-09-21 11:58:29 -0600

    Send routes through Serializer

--- app/controllers/api/v1/climbing_routes_controller.rb
+++ app/controllers/api/v1/climbing_routes_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::ClimbingRoutesController < ApplicationController
   def index
     location = params[:location]
     routes = ClimbingRoutesFacade.new(location)
+    render json: ClimbingRouteSerializer.new(routes)
   end
 end 
