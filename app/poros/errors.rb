b1b01343692c47a3fc8d6595cc12b1ce027a2f31
Josh Tukman <josh.tukman@gmail.com>
2020-09-22 16:00:21 -0600

    Add invalid_password and invalid_credentials methods

--- app/poros/errors.rb
+++ app/poros/errors.rb
@@ -7,5 +7,11 @@ class Errors
   end
   def no_image
     { error: "Your search returned no images" }
-  end 
+  end
+  def invalid_password
+    { error: "Invalid Password", status: 401}
+  end
+  def invalid_credentials
+    { error: "Invalid Credentials", status: 404}
+  end
 end 
