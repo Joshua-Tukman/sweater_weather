56361f1cf1a76da6247e5e547075dd351b27169d
Josh Tukman <josh.tukman@gmail.com>
2020-09-21 19:57:30 -0600

    Create poro with required attributes

---
+++ app/poros/background_image.rb
@@ -0,0 +1,11 @@
+class BackgroundImage
+  attr_reader :location, :image_url, :id
+  def initialize(image_info, location)  
+    @location = location
+    @image_url = image_formatted(image_info)
+  end
+
+  def image_formatted(image_info)
+    {image_url: image_info.first[:largeImageURL], credit: {source: "pixabay.com", author: image_info.first[:user], logo: "https://pixabay.com/static/img/logo_square.png"}}
+  end
+end 
