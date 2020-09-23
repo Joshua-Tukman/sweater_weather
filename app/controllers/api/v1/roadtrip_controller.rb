4fd3acdbbf53ba6649f7b61c32b8c36dea23fa73
Josh Tukman <josh.tukman@gmail.com>
2020-09-22 23:29:36 -0600

    Create sad paths for when city is invalid in road trip

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -2,11 +2,14 @@ class Api::V1::RoadtripController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
     if user.nil?
-      render json: Errors.new.api_key_not_found, status: 401
+      message = render json: Errors.new.api_key_not_found, status: 401
     else  
-      roadtrip = RoadtripFacade.new(params[:origin], params[:destination])   
-    end
-     
-    render json: RoadtripSerializer.new(roadtrip), status: 201
+      roadtrip = RoadtripFacade.new(params[:origin], params[:destination])
+      if roadtrip.travel_time.class == Hash || roadtrip.travel_time == Hash
+        render json: roadtrip.travel_time, status: 401
+      else  
+        render json: RoadtripSerializer.new(roadtrip), status: 201
+      end 
+    end 
   end
 end 
