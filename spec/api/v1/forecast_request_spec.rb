a5ce0378230379a5a04d43d060d5eb6b4d534866
Josh Tukman <josh.tukman@gmail.com>
2020-09-22 23:32:01 -0600

    Update tests to reflect new fixtures

--- spec/api/v1/forecast_request_spec.rb
+++ spec/api/v1/forecast_request_spec.rb
@@ -11,31 +11,31 @@ RSpec.describe 'Forecast request' do
       json = JSON.parse(response.body, symbolize_names: true)
      
       expect(json[:data][:attributes][:forecast].keys).to include(:time)
-      expect(json[:data][:attributes][:forecast][:time]).to eq("1:56 PM, September 20")
-      expect(json[:data][:attributes][:forecast][:current_temp]).to eq(65)
-      expect(json[:data][:attributes][:forecast][:feels_like]).to eq(64)
-      expect(json[:data][:attributes][:forecast][:humidity]).to eq(93)
+      expect(json[:data][:attributes][:forecast][:time]).to eq("11:18 PM, September 22")
+      expect(json[:data][:attributes][:forecast][:current_temp]).to eq(70)
+      expect(json[:data][:attributes][:forecast][:feels_like]).to eq(71)
+      expect(json[:data][:attributes][:forecast][:humidity]).to eq(78)
       expect(json[:data][:attributes][:forecast][:visibility]).to eq(10000)
-      expect(json[:data][:attributes][:forecast][:uv_index]).to eq(9)
-      expect(json[:data][:attributes][:forecast][:sunrise]).to eq("5:33 AM")
-      expect(json[:data][:attributes][:forecast][:sunset]).to eq("5:45 PM")
-      expect(json[:data][:attributes][:forecast][:high_temp]).to eq(69)
-      expect(json[:data][:attributes][:forecast][:low_temp]).to eq(64)
+      expect(json[:data][:attributes][:forecast][:uv_index]).to eq(8)
+      expect(json[:data][:attributes][:forecast][:sunrise]).to eq("5:35 AM")
+      expect(json[:data][:attributes][:forecast][:sunset]).to eq("5:41 PM")
+      expect(json[:data][:attributes][:forecast][:high_temp]).to eq(80)
+      expect(json[:data][:attributes][:forecast][:low_temp]).to eq(71)
       #test hourly info
       expect(json[:data][:attributes][:forecast][:hourly].count).to eq(24)
       expect(json[:data][:attributes][:forecast][:hourly].first.keys).to include(:temp)
       expect(json[:data][:attributes][:forecast][:hourly].first.keys).to include(:time)
-      expect(json[:data][:attributes][:forecast][:hourly].first[:temp]).to eq(65)
-      expect(json[:data][:attributes][:forecast][:hourly].first[:time]).to eq("1 PM")
+      expect(json[:data][:attributes][:forecast][:hourly].first[:temp]).to eq(70)
+      expect(json[:data][:attributes][:forecast][:hourly].first[:time]).to eq("11 PM")
       #test daily info 
       expect(json[:data][:attributes][:forecast][:five_day_forecast].count).to eq(5)
-      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:day]).to eq("Monday")
-      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:description]).to eq("Clouds")
+      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:day]).to eq("Thursday")
+      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:description]).to eq("Rain")
       rain = json[:data][:attributes][:forecast][:five_day_forecast][1][:rain][:rain]
       snow = json[:data][:attributes][:forecast][:five_day_forecast][1][:snow][:snow]
       expect(json[:data][:attributes][:forecast][:five_day_forecast][1][:precipitation]).to eq(rain + snow)
-      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:high_temp]).to eq(77)
-      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:low_temp]).to eq(68)
+      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:high_temp]).to eq(80)
+      expect(json[:data][:attributes][:forecast][:five_day_forecast].first[:low_temp]).to eq(76)
     end          
   end
   it "returns an error when the city is not valid" do
